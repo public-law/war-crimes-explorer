6e65d26ebd3f1a228770359554ae1f5bce9daf35
Robb Shecter <robb@public.law>
2023-11-30 16:19:39 -0700

    cleanup

--- crimes.pl
+++ crimes.pl
@@ -28,15 +28,15 @@ genocidal_act(D, G) :- act(D, imposed_on_members, G).
 genocidal_act(D, G) :- act(D, forcibly_transferred_children_of_members, G).
 
 /* Test data */
-genocidal_intent(sudan, fur).
-genocidal_intent(sudan, masalit).
-genocidal_intent(sudan, zaghawa).
-genocidal_intent(sudan, tunjur).
-
 group(fur).
 group(masalit).
 group(zaghawa).
 group(tunjur).
+
+genocidal_intent(sudan, fur).
+genocidal_intent(sudan, masalit).
+genocidal_intent(sudan, zaghawa).
+genocidal_intent(sudan, tunjur).
 
 act(sudan, killed_members, fur).
 act(sudan, killed_members, masalit).
@@ -55,21 +55,21 @@ act(sudan, killed_members, tunjur).
  * A first, simple attempt at Protected Persons under
  * the Geneva Conventions of 1949.
  */
-protected_by_geneva_convention(P) :- person(P), civilian(P).
-protected_by_geneva_convention(P) :- person(P), prisoner_of_war(P).
-protected_by_geneva_convention(P) :- person(P), medical_personnel(P).
-protected_by_geneva_convention(P) :- person(P), religious_personnel(P).
+% protected_by_geneva_convention(P) :- person(P), civilian(P).
+% protected_by_geneva_convention(P) :- person(P), prisoner_of_war(P).
+% protected_by_geneva_convention(P) :- person(P), medical_personnel(P).
+% protected_by_geneva_convention(P) :- person(P), religious_personnel(P).
 
-criminal_liability(war_crime, Statute, D) :-
-	protected_by_geneva_convention(V),
-	international_conflict(D, V),
-	elements(Statute, D, V).
+% criminal_liability(war_crime, Statute, D) :-
+% 	protected_by_geneva_convention(V),
+% 	international_conflict(D, V),
+% 	elements(Statute, D, V).
 
-elements(article_8_2_a_i, D, V) :-
-	act(D, killed, V).
+% elements(article_8_2_a_i, D, V) :-
+% 	act(D, killed, V).
 
-elements(article_8_2_a_ii, D, V) :-
-	act(D, tortured, V).
+% elements(article_8_2_a_ii, D, V) :-
+% 	act(D, tortured, V).
 
 
 
