f88cee74c8577b777021559529d8028e5cf2afe8
Robb Shecter <robb@public.law>
2023-11-30 16:07:12 -0700

    finished first pass

--- crimes.pl
+++ crimes.pl
@@ -10,36 +10,60 @@ crime(crime_of_aggression).
 
 
 /*
- * A first, simple attempt at Protected Persons under
- * the Geneva Conventions of 1949.
- */
-protected_by_geneva_convention(P) :- civilian(P).
-protected_by_geneva_convention(P) :- prisoner_of_war(P).
-protected_by_geneva_convention(P) :- medical_personnel(P).
-protected_by_geneva_convention(P) :- religious_personnel(P).
-
-
-/*
- * D = Defendant
- * V = Victim
- */
-
-/*
  * Genocide
  * https://world.public.law/rome_statute/article_6_genocide
  */
-criminal_liability(genocide, Statute, D, V) :-
-	elements(Statute, D, V).
+criminal_liability(genocide, D, G) :-
+	group(G),
+	elements(genocide, D, G).
+
+elements(genocide, D, G) :-
+	genocidal_intent(D, G),
+	genocidal_act(D, G).
+
+genocidal_act(D, G) :- act(D, killed_members, G).
+genocidal_act(D, G) :- act(D, caused_serious_bodily_harm_to_members, G).
+genocidal_act(D, G) :- act(D, deliberately_inflicted_on_members, G).
+genocidal_act(D, G) :- act(D, imposed_on_members, G).
+genocidal_act(D, G) :- act(D, forcibly_transferred_children_of_members, G).
+
+/* Test data */
+genocidal_intent(darfur, fur).
+genocidal_intent(darfur, masalit).
+genocidal_intent(darfur, zaghawa).
+genocidal_intent(darfur, tunjur).
+
+group(fur).
+group(masalit).
+group(zaghawa).
+group(tunjur).
+
+act(darfur, killed_members, fur).
+act(darfur, killed_members, masalit).
+act(darfur, killed_members, zaghawa).
+act(darfur, killed_members, tunjur).
+
+
+
 
 /*
  * War crimes
  * https://world.public.law/rome_statute/article_8_war_crimes
  */
-criminal_liability(war_crime, Statute, D, V) :-
+
+/*
+ * A first, simple attempt at Protected Persons under
+ * the Geneva Conventions of 1949.
+ */
+protected_by_geneva_convention(P) :- person(P), civilian(P).
+protected_by_geneva_convention(P) :- person(P), prisoner_of_war(P).
+protected_by_geneva_convention(P) :- person(P), medical_personnel(P).
+protected_by_geneva_convention(P) :- person(P), religious_personnel(P).
+
+criminal_liability(war_crime, Statute, D) :-
 	protected_by_geneva_convention(V),
 	international_conflict(D, V),
 	elements(Statute, D, V).
-
 
 elements(article_8_2_a_i, D, V) :-
 	act(D, killed, V).
@@ -48,10 +72,15 @@ elements(article_8_2_a_ii, D, V) :-
 	act(D, tortured, V).
 
 
-:- dynamic act/3.
-:- dynamic international_conflict/2.
 
+:- dynamic genocidal_intent/2.
+:- dynamic group/1.
+
+:- dynamic act/3.
+
+:- dynamic international_conflict/2.
 :- dynamic civilian/1.
+:- dynamic person/1.
 :- dynamic prisoner_of_war/1.
 :- dynamic medical_personnel/1.
 :- dynamic religious_personnel/1.
