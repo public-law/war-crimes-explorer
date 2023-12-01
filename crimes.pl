be794ca84dce2fc2f44950e4e36cd69e6768cac0
Robb Shecter <robb@public.law>
2023-12-01 12:05:06 -0700

    Further developing the crime of genocide (#3)
    * finished first pass
    
    * fix: test data
    
    * cleanup
    
    * checkpoint
    
    * focus on genocide
    
    * add jur but w/out gen. intent
    
    * refactor: naming
    
    * commit add more Sudanese ethnic groups
    
    * more groups
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint

--- crimes.pl
+++ crimes.pl
@@ -1,57 +1,81 @@
 /*
- * Crimes within the jurisdiction of the International Criminal Court.
- * 
- * https://world.public.law/rome_statute/article_5_crimes_within_the_jurisdiction_of_the_court
- */
-crime(genocide).
-crime(war_crime).
-crime(crime_against_humanity).
-crime(crime_of_aggression).
-
-
-/*
- * A first, simple attempt at Protected Persons under
- * the Geneva Conventions of 1949.
- */
-protected_by_geneva_convention(P) :- civilian(P).
-protected_by_geneva_convention(P) :- prisoner_of_war(P).
-protected_by_geneva_convention(P) :- medical_personnel(P).
-protected_by_geneva_convention(P) :- religious_personnel(P).
-
-
-/*
- * D = Defendant
- * V = Victim
- */
-
-/*
  * Genocide
  * https://world.public.law/rome_statute/article_6_genocide
  */
-criminal_liability(genocide, Statute, D, V) :-
-	elements(Statute, D, V).
+criminal_liability(genocide, Accused, Group) :-
+	genocidal_intent(Accused, Group),
+	article_6_group(Group),
+	genocidal_act(Accused, Group).
+
+article_6_group(Group) :- national_group(Group).
+article_6_group(Group) :- ethnic_group(Group).
+article_6_group(Group) :- racial_group(Group).
+article_6_group(Group) :- religious_group(Group).
+
+genocidal_act(Accused, Group) :- act(Accused, killed, Group).
+genocidal_act(Accused, Group) :- act(Accused, caused_serious_bodily_harm, Group).
+genocidal_act(Accused, Group) :- act(Accused, deliberately_inflicted_conditions, Group).
+genocidal_act(Accused, Group) :- act(Accused, imposed_measures_to_prevent_births, Group).
+genocidal_act(Accused, Group) :- act(Accused, forcibly_transferred_children, Group).
+
 
 /*
- * War crimes
- * https://world.public.law/rome_statute/article_8_war_crimes
+ * Test data 
+ * 
+ * Normally, this would be provided by the user via a web interface
+ * or dialog with a chatbot.
  */
-criminal_liability(war_crime, Statute, D, V) :-
-	protected_by_geneva_convention(V),
-	international_conflict(D, V),
-	elements(Statute, D, V).
+
+/* Acts that were committed */
+act(sudan, killed, beja).
+act(sudan, killed, fur).
+act(sudan, killed, masalit).
+act(sudan, killed, nuba).
+act(sudan, killed, christian).
+act(sudan, killed, zaghawa).
+act(sudan, caused_serious_bodily_harm, fur).
+act(sudan, encouraged_to_temporarily_relocate, dajo).
+act(sudan, encouraged_to_temporarily_relocate, fur).
+act(sudan, encouraged_to_temporarily_relocate, uduk).
+act(sudan, encouraged_to_temporarily_relocate, arab).
+
+/* Sudanese groups */
+ethnic_group(arab).
+ethnic_group(beja).
+ethnic_group(berti).
+ethnic_group(dajo).
+ethnic_group(fallata).
+ethnic_group(fur).
+ethnic_group(gimir).
+ethnic_group(ingessana).
+ethnic_group(jur).
+ethnic_group(masalit).
+ethnic_group(nuba).
+ethnic_group(tunjur).
+ethnic_group(uduk).
+ethnic_group(zaghawa).
+
+religious_group(sunni_muslim).
+religious_group(christian).
+
+/* Sudan has/had the goal of destroying these groups, i.e., ethnic cleansing. */
+genocidal_intent(sudan, fallata).
+genocidal_intent(sudan, fur).
+genocidal_intent(sudan, jur).
+genocidal_intent(sudan, masalit).
+genocidal_intent(sudan, nuba).
+genocidal_intent(sudan, tunjur).
+genocidal_intent(sudan, zaghawa).
 
 
-elements(article_8_2_a_i, D, V) :-
-	act(D, killed, V).
+/*
+ * Configuration
+ */
+:- dynamic genocidal_intent/2.
 
-elements(article_8_2_a_ii, D, V) :-
-	act(D, tortured, V).
-
+:- dynamic national_group/1.
+:- dynamic ethnic_group/1.
+:- dynamic racial_group/1.
+:- dynamic religious_group/1.
 
 :- dynamic act/3.
-:- dynamic international_conflict/2.
-
-:- dynamic civilian/1.
-:- dynamic prisoner_of_war/1.
-:- dynamic medical_personnel/1.
-:- dynamic religious_personnel/1.
