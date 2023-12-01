ab2d440abf7093c134f13dc8cc0c9e27cbf34f11
Robb Shecter <robb@public.law>
2023-12-01 10:11:14 -0700

    focus on genocide

--- crimes.pl
+++ crimes.pl
@@ -1,12 +1,14 @@
 /*
- * Crimes within the jurisdiction of the International Criminal Court.
- * 
- * https://world.public.law/rome_statute/article_5_crimes_within_the_jurisdiction_of_the_court
+ * Configuration
  */
-crime(genocide).
-crime(war_crime).
-crime(crime_against_humanity).
-crime(crime_of_aggression).
+:- dynamic genocidal_intent/2.
+
+:- dynamic national_group/1.
+:- dynamic ethnic_group/1.
+:- dynamic racial_group/1.
+:- dynamic religious_group/1.
+
+:- dynamic act/3.
 
 
 /*
@@ -18,7 +20,7 @@ criminal_liability(genocide, D, G) :-
 	elements(genocide, D, G).
 
 group(G) :- national_group(G).
-group(G) :- ethnical_group(G).
+group(G) :- ethnic_group(G).
 group(G) :- racial_group(G).
 group(G) :- religious_group(G).
 
@@ -33,9 +35,9 @@ genocidal_act(D, G) :- act(D, imposed_on_members, G).
 genocidal_act(D, G) :- act(D, forcibly_transferred_children_of_members, G).
 
 /* Test data */
-ethnical_group(fur).
-ethnical_group(masalit).
-ethnical_group(zaghawa).
+ethnic_group(fur).
+ethnic_group(masalit).
+ethnic_group(zaghawa).
 
 genocidal_intent(sudan, fur).
 genocidal_intent(sudan, masalit).
@@ -44,48 +46,3 @@ genocidal_intent(sudan, zaghawa).
 act(sudan, killed_members, fur).
 act(sudan, killed_members, masalit).
 act(sudan, killed_members, zaghawa).
-
-
-/*
- * War crimes
- * https://world.public.law/rome_statute/article_8_war_crimes
- */
-
-/*
- * A first, simple attempt at Protected Persons under
- * the Geneva Conventions of 1949.
- */
-% protected_by_geneva_convention(P) :- person(P), civilian(P).
-% protected_by_geneva_convention(P) :- person(P), prisoner_of_war(P).
-% protected_by_geneva_convention(P) :- person(P), medical_personnel(P).
-% protected_by_geneva_convention(P) :- person(P), religious_personnel(P).
-
-% criminal_liability(war_crime, Statute, D) :-
-% 	protected_by_geneva_convention(V),
-% 	international_conflict(D, V),
-% 	elements(Statute, D, V).
-
-% elements(article_8_2_a_i, D, V) :-
-% 	act(D, killed, V).
-
-% elements(article_8_2_a_ii, D, V) :-
-% 	act(D, tortured, V).
-
-
-
-:- dynamic genocidal_intent/2.
-
-:- dynamic national_group/1.
-:- dynamic ethnical_group/1.
-:- dynamic racial_group/1.
-:- dynamic religious_group/1.
-
-
-:- dynamic act/3.
-
-:- dynamic international_conflict/2.
-:- dynamic civilian/1.
-:- dynamic person/1.
-:- dynamic prisoner_of_war/1.
-:- dynamic medical_personnel/1.
-:- dynamic religious_personnel/1.
